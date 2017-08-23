fc266d3c2bc11b0462c118b7cb2a82af19e080e4
ZoroZeng <aplexos@163.com>
2017-08-23 12:37:26 +0800

    add code hightlight.

--- js/zengjf_codeframe.js
+++ js/zengjf_codeframe.js
@@ -1,10 +1,28 @@
+function clean_show_content() {
+    $('#show-content').html(
+        "<div class=\"row clearfix\"> \
+                <div id=\"show-content_left\" class=\"show_content_left_div col-md-6 column\"> \
+                </div> \
+                <div id=\"show-content_right\" class=\"show_content_right_div col-md-6 column\"> \
+                  <pre> \
+                    <code id=\"show-content_code\" class=\"html\"> \
+                    </code> \
+                  </pre> \
+                </div> \
+        </div>"
+    );
+}
 function readHTML(){ 
+    clean_show_content();
+
     $.ajax({ 
         async:false, 
-        url : "index.html", 
+        url : "svg/javascript_control_svg_elemet.html", 
         success : function(result){ 
-            //$('#show-content').html(result);
+            $('#show-content_left').html(result);
+            $('#show-content_code').html(result);
             console.info(result);
+            hljs.initHighlightingOnLoad();
         } 
        
     }); 
